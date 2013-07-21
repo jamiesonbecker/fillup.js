b6f1efc34aa5e093951be80eaee95427017c4544
Jamieson Becker <jamieson@jamiesonbecker.com>
2013-07-20 22:47:28 -0500

    cleaned code a bit too well

--- fillup.js
+++ fillup.js
@@ -2,4 +2,4 @@
 // https://github.com/jamiesonbecker/fillup.js
 // Copyright (c) 2013 Jamieson Becker (MIT)
 
-;$.fn.fillup = function(src){return $(this).empty().html($("<div/>").addClass(src.slice(1)).html($(src + "-offstage").children().clone(false)))}
+;$.fn.fillup = function(src){return $(this).empty().html($("<div/>").addClass(src.slice(1)).html($("." + src + "-offstage").children().clone(false)))}
